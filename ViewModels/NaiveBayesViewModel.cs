6c3ab956561114fb3810c5208ec62aad795925f9
Martin Bodin <martin@mbodin.se>
2017-08-21 11:03:57 +0200

    Initial commit

---
+++ ViewModels/NaiveBayesViewModel.cs
@@ -0,0 +1,51 @@
+﻿using NaiveBayesDemo.Common;
+using System;
+using System.Windows.Input;
+
+namespace NaiveBayesDemo.ViewModels
+{
+    public class NaiveBayesViewModel : ViewModelBase
+    {
+
+        public NaiveBayesViewModel()
+        {
+            //Instantiate all commands here
+            ClassifyTextCommand = new DelegateCommand(ClassifyText);
+            TrainCommand = new DelegateCommand(Train);
+            TestCommand = new DelegateCommand(Test);
+        }
+
+        private string _textToClassify = "";
+
+        //PROPERTIES
+
+        public String TextToClassify
+        {
+            get { return _textToClassify; }
+            set { Set(ref _textToClassify, value); }
+        }
+
+        //COMMANDS
+        
+        public ICommand ClassifyTextCommand { get; private set; }
+
+        private void ClassifyText()
+        {
+            TextToClassify += "ClassifyCommand ";
+        }
+
+        public ICommand TrainCommand { get; private set; }
+
+        private void Train()
+        {
+            TextToClassify += "TrainCommand ";
+        }
+
+        public ICommand TestCommand { get; private set; }
+
+        private void Test()
+        {
+            TextToClassify = "TRIGGER";
+        }
+    }
+}
---
+++ Views/NaiveBayesView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using NaiveBayesDemo.ViewModels;
+using System.Windows.Controls;
+
+namespace NaiveBayesDemo.Views
+{
+    /// <summary>
+    /// Interaction logic for NaiveBayesView.xaml
+    /// </summary>
+    public partial class NaiveBayesView : UserControl
+    {
+        public NaiveBayesView()
+        {
+            InitializeComponent();
+        }
+
+        protected NaiveBayesViewModel Model
+        {
+            get { return (NaiveBayesViewModel)Resources["ViewModel"]; }
+        }
+    }
+}
